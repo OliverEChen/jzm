3ba9002e7be33a3c340f11b1a80900aea20e5486
chenlong <oliverechen@163.com>
2023-07-25 21:06:16 +0800

    commit

---
+++ pages/index/data.js
@@ -0,0 +1,721 @@
+export const data = [{
+		"carNo": "皖KNF071蓝",
+		"carType": "多用途车(MPV)",
+		"ton": "7",
+		"range": "网络预约出租汽车客运",
+		"model": "大通牌SH6525C1DC-A",
+		"firstDate": "2022/1/21",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KPR720蓝",
+		"carType": "多用途车(MPV)",
+		"ton": "7",
+		"range": "网络预约出租汽车客运",
+		"model": "大通牌SH6525C1DC-A",
+		"firstDate": "2021/11/24",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KPT912蓝",
+		"carType": "多用途车(MPV)",
+		"ton": "7",
+		"range": "网络预约出租汽车客运",
+		"model": "大通牌SH6525C1DC-A",
+		"firstDate": "2021/11/24",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KPV397蓝",
+		"carType": "多用途车(MPV)",
+		"ton": "7",
+		"range": "网络预约出租汽车客运",
+		"model": "大通牌SH6525C1DC-A",
+		"firstDate": "2022/1/20",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD26933绿",
+		"carType": "多用途车(MPV)",
+		"ton": "7",
+		"range": "网络预约出租汽车客运",
+		"model": "开瑞牌SQR6460BEVK09",
+		"firstDate": "2021/6/2",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD23836绿",
+		"carType": "多用途车(MPV)",
+		"ton": "7",
+		"range": "网络预约出租汽车客运",
+		"model": "开瑞牌SQR6460BEVK09",
+		"firstDate": "2021/6/4",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD27787绿",
+		"carType": "多用途车(MPV)",
+		"ton": "7",
+		"range": "网络预约出租汽车客运",
+		"model": "开瑞牌SQR6460BEVK09",
+		"firstDate": "2021/6/4",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD26918绿",
+		"carType": "多用途车(MPV)",
+		"ton": "7",
+		"range": "网络预约出租汽车客运",
+		"model": "开瑞牌SQR6460BEVK09",
+		"firstDate": "2021/6/2",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD28895绿",
+		"carType": "多用途车(MPV)",
+		"ton": "7",
+		"range": "网络预约出租汽车客运",
+		"model": "开瑞牌SQR6460BEVK09",
+		"firstDate": "2021/6/4",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD23085绿",
+		"carType": "多用途车(MPV)",
+		"ton": "7",
+		"range": "网络预约出租汽车客运",
+		"model": "开瑞牌SQR6460BEVK09",
+		"firstDate": "2021/6/2",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD20385绿",
+		"carType": "多用途车(MPV)",
+		"ton": "7",
+		"range": "网络预约出租汽车客运",
+		"model": "开瑞牌SQR6460BEVK09",
+		"firstDate": "2021/6/4",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD23226绿",
+		"carType": "多用途车(MPV)",
+		"ton": "7",
+		"range": "网络预约出租汽车客运",
+		"model": "开瑞牌SQR6460BEVK09",
+		"firstDate": "2021/6/2",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KUM953蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "东风雪铁龙DC7162LYEM",
+		"firstDate": "2021/11/12",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KQX537蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "北京现代牌BH7160VAS",
+		"firstDate": "2021/11/16",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KSW785蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "东风雪铁龙DC7162LYEM",
+		"firstDate": "2021/10/19",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KEH173蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "东风雪铁龙DC7162LYEM",
+		"firstDate": "2021/10/21",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD86600绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "雪佛兰牌SGM7007LBBEV",
+		"firstDate": "2022/3/4",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KUF836蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "起亚牌YQZ7169AMGE5",
+		"firstDate": "2021/10/21",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KGU937蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "起亚牌YQZ7169AMGE5",
+		"firstDate": "2021/11/22",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KRE838蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "起亚牌YQZ7169MGE5",
+		"firstDate": "2021/11/4",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KDD9001绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "比亚迪BYD7002BEVA9",
+		"firstDate": "2022/12/22",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KNC325蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "斯柯达牌SVW71615CM",
+		"firstDate": "2021/12/8",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD58082绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "合创牌GAH6460BEVSOF",
+		"firstDate": "2023/7/4",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖K58978蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "长安牌SC7154CAB6",
+		"firstDate": "2023/6/28",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KFD536蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "起亚牌YQZ7161ADE6",
+		"firstDate": "2021/1/4",
+		"status": "注销",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖K83EB6蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "起亚牌YQZ7161ADE6",
+		"firstDate": "2021/7/15",
+		"status": "营运",
+		"company": "武汉斑马快跑科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD99588绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "哪吒牌THZ7000BEVS117",
+		"firstDate": "2022/11/2",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD97086绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "力帆牌LF7002PEV",
+		"firstDate": "2022/9/19",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KDD7030绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "比亚迪牌BYD7003BEV10",
+		"firstDate": "2022/9/8",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KDD9398绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "比亚迪牌BYD7002BEVA9",
+		"firstDate": "2022/11/2",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖K0C502蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "起亚牌YQZ7161ADE6",
+		"firstDate": "2022/2/15",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KXK532蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "北京现代牌BH7160TMV",
+		"firstDate": "2022/6/8",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KQP713蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "东风雪铁龙牌DC7162LYEM",
+		"firstDate": "2022/5/19",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KAG881蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "大众牌FV7146NBDCG",
+		"firstDate": "2022/7/28",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD00355绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "比亚迪牌BYD7003BEV8",
+		"firstDate": "2021/11/16",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KSR776蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "北京现代牌BH7160TMV",
+		"firstDate": "2022/5/19",
+		"status": "注销",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KDD6888绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "东风牌LZ7005SLAEV",
+		"firstDate": "2022/10/13",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD91677绿",
+		"carType": "运动型多用途车(SUV)",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "哪吒牌THZ6450BEVS309",
+		"firstDate": "2022/11/2",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KBJ251蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "丰田牌TV7184GL- iHEV",
+		"firstDate": "2022/3/15",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KSW071蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "北京现代牌BH7160VAS",
+		"firstDate": "2022/6/17",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD00191绿",
+		"carType": "运动型多用途车(SUV)",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "合创牌GAH6460BEVS0A",
+		"firstDate": "2023/4/7",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KDD3515绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "埃安牌GAM7001BEVA0N",
+		"firstDate": "2023/6/13",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD67107绿",
+		"carType": "运动型多用途车(SUV)",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "埃安牌GAM6450BEVD0E",
+		"firstDate": "2023/4/25",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD59023绿",
+		"carType": "运动型多用途车(SUV)",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "哪吒牌THZ6450BEVS309",
+		"firstDate": "2023/4/25",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD73729绿",
+		"carType": "运动型多用途车(SUV)",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "埃安牌GAM6450BEVD0N",
+		"firstDate": "2023/5/18",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KDG8052绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "比亚迪牌BYD7005BEVA8",
+		"firstDate": "2023/5/31",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KDE3028绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "埃安牌GAM7001BEVA0W",
+		"firstDate": "2023/6/13",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD88598绿",
+		"carType": "运动型多用途车(SUV)",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "比亚迪牌BYD7003BEVA3",
+		"firstDate": "2023/4/14",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD83050绿",
+		"carType": "运动型多用途车(SUV)",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "哪吒牌THZ6450BEVS309",
+		"firstDate": "2023/2/27",
+		"status": "营运",
+		"company": "深圳万顺叫车云信息技术有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KAQ271蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "斯柯达牌SVW71615CM",
+		"firstDate": "2021/11/26",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KPH738蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "东风雪铁龙DC7162LYEA",
+		"firstDate": "2021/11/26",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KSW270蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "北京现代牌BH7160VAV",
+		"firstDate": "2021/11/26",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KRX107蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "斯柯达牌SVW71613AS",
+		"firstDate": "2021/11/12",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖K3071J蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "丰田牌TV7187EHEV6",
+		"firstDate": "2022/3/14",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖K8V579蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "东风日产牌DFL7160VBNH7",
+		"firstDate": "2022/7/29",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖K8361R蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "北京现代牌BH7160VAS",
+		"firstDate": "2021/11/22",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KRC278蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "东风雪铁龙牌DC7162LYEM",
+		"firstDate": "2021/11/22",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KWS271蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "斯柯达牌SVW71615EM",
+		"firstDate": "2022/9/15",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KRB717蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "斯柯达牌SVW71613AS",
+		"firstDate": "2022/9/6",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KVY928蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "起亚牌YQZ7169MGE5",
+		"firstDate": "2022/11/24",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KSB629蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "东风雪铁龙DC7162LYEM",
+		"firstDate": "2021/11/22",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖K72T15蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "东风日产DFL7160VALH1",
+		"firstDate": "2022/3/3",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KPU252蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "起亚牌YQZ7169AMGE5",
+		"firstDate": "2021/12/16",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KVT557蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "起亚牌YQZ7150ANE6",
+		"firstDate": "2022/9/9",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KAG862蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "大众汽车牌SVW71617SM",
+		"firstDate": "2022/9/30",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KC6659蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "北京现代牌BH7160VAV",
+		"firstDate": "2022/9/30",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KPB886蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "北京现代牌BH160VAS",
+		"firstDate": "2022/7/15",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KJ329Q蓝",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "北京现代牌BH7162QAV",
+		"firstDate": "2022/9/20",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KD02688绿",
+		"carType": "小型轿车",
+		"ton": "6",
+		"range": "网络预约出租汽车客运",
+		"model": "猎豹牌LBA6431BABEV",
+		"firstDate": "2022/9/16",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KF80110绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "比亚迪牌BYD7152WT6HEVB14",
+		"firstDate": "2022/8/10",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KDE0759绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "荣威牌CSA7001SBEV4",
+		"firstDate": "2023/4/24",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	},
+	{
+		"carNo": "皖KDE9529绿",
+		"carType": "小型轿车",
+		"ton": "5",
+		"range": "网络预约出租汽车客运",
+		"model": "埃安牌GAM6450BEVD0N",
+		"firstDate": "2023/6/28",
+		"status": "营运",
+		"company": "安徽华格科技有限公司颍上分公司"
+	}
+]
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,120 +1,157 @@
 <template>
 	<view class="page">
-		<view class="banner">
-			<image src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/shuijiao.jpg" mode="widthFix"></image>
-		</view>
+		<view class="banner"><image src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/shuijiao.jpg" mode="widthFix"></image></view>
 		<view style="padding: 0 20rpx; transform: translateY(-100rpx);">
 			<view class="form">
-				<input type="text" placeholder="请输入车牌号" v-model="carNo">
+				<input type="text" placeholder="请输入车牌号" v-model="carNo" />
 				<button @tap="handleQuery">点击查询</button>
 			</view>
 			<view style="text-align: center;margin-bottom: 20rpx;">--查询结果--</view>
 			<template v-for="item in queryData">
 				<view class="card">
-					<view>车牌号码：{{item.carNo}}</view>
-					<view style="border: 1px solid #ccc;margin: 10rpx;"></view>
-					<view>已取得网约车运输证</view>
-					<view style="font-size: 24rpx;">(许可地：北京)</view>
+					<view class="flex" style="width: 80vw;overflow: auto;">
+						<view class="carNo flex common">
+							<view class="label">车牌号</view>
+							<view>{{ item.carNo }}</view>
+						</view>
+						<view class="seat flex common">
+							<view class="label">吨(座)位</view>
+							<view>{{ item.ton }}</view>
+						</view>
+						<!-- <view class="brand flex common">
+							<view class="label">品牌</view>
+							<view>品牌</view>
+						</view> -->
+					</view>
+					<view class="flex">
+						<view class="title">经营范围:</view>
+						<view class="text">{{ item.range }}</view>
+					</view>
+					<view class="flex">
+						<view class="title">首次发证日期:</view>
+						<view class="text">{{ item.firstDate }}</view>
+					</view>
+					<view class="flex">
+						<view class="title">厂牌型号:</view>
+						<view class="text">{{ item.model }}</view>
+					</view>
+					<view class="flex">
+						<view class="title">车辆类型:</view>
+						<view class="text">{{ item.carType }}</view>
+					</view>
+					<view class="flex">
+						<view class="title">车辆状态:</view>
+						<view class="text">{{ item.status }}</view>
+					</view>
+					<view class="flex">
+						<view class="title">所属公司:</view>
+						<view class="text">{{ item.company }}</view>
+					</view>
 				</view>
 			</template>
-
 		</view>
-
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello',
-				carNo: '',
-				queryData: [],
-				dataSource: [{
-						no: '租341226800016',
-						carNo: '皖KUM953蓝',
-						carType: '小型轿车',
-						ton: 5,
-						range: '网络预约出租汽车客运',
-						model: '东风雪铁龙DC7162LYEM',
-						firstDate: '11/12/21',
-						status: '营运'
-					},
-					{
-						no: '租341226800016',
-						carNo: '贵FUM953蓝',
-						carType: '小型轿车',
-						ton: 5,
-						range: '网络预约出租汽车客运',
-						model: '东风雪铁龙DC7162LYEM',
-						firstDate: '11/12/21',
-						status: '营运'
-					},
-					{
-						no: '租341226800016',
-						carNo: '沪CUM953蓝',
-						carType: '小型轿车',
-						ton: 5,
-						range: '网络预约出租汽车客运',
-						model: '东风雪铁龙DC7162LYEM',
-						firstDate: '11/12/21',
-						status: '营运'
-					}
-				]
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-			handleQuery() {
-				console.log(this.carNo)
-				if(this.carNo){
-					this.queryData = this.dataSource.filter((item) => item.carNo.includes(this.carNo))
-				}else {
-					this.queryData = []
-				}
-
+import { data } from './data.js';
+export default {
+	data() {
+		return {
+			title: 'Hello',
+			carNo: '皖KDE9529绿',
+			queryData: [],
+			dataSource: []
+		};
+	},
+	onLoad() {},
+	methods: {
+		handleQuery() {
+			console.log(this.carNo);
+			if (this.carNo) {
+				this.queryData = data.filter(item => item.carNo.includes(this.carNo));
+			} else {
+				this.queryData = [];
 			}
 		}
 	}
+};
 </script>
 
 <style lang="scss">
-	.page {
-		.card {
-			background-color: #bed5f4;
-			padding: 20rpx;
-			margin-bottom: 10rpx;
+.text {
+	color: #4d77ab;
+}
+.flex {
+	display: flex;
+}
+.common {
+	& > view {
+		padding: 5rpx 10rpx;
+	}
+}
+.carNo {
+	.label {
+		color: #fff;
+		background: #c69905;
+	}
+	color: #c69905;
+	border: 1px solid #c69905;
+}
+.seat {
+	.label {
+		color: #fff;
+		background: #c65f06;
+	}
+	color: #c65f06;
+	border: 1px solid #c65f06;
+	margin: 0 10rpx;
+}
+.brand {
+	.label {
+		color: #fff;
+		background: #3d71b8;
+	}
+	color: #3d71b8;
+	border: 1px solid #3d71b8;
+}
+.label {
+}
+.page {
+	.card {
+		border: 1px solid #ccc;
+		padding: 20rpx;
+		margin-bottom: 10rpx;
 
-			&>view {
-				margin: 6rpx 0;
-			}
-		}
-
-		.form {
-			padding: 20rpx;
-			background-color: #fff;
-
-			input {
-				height: 80rpx;
-				border: 2rpx solid #244FD6;
-				border-radius: 20rpx;
-			}
-
-			button {
-				margin-top: 30rpx;
-				height: 80rpx;
-				background: #244FD6;
-				border-radius: 40rpx;
-				color: #fff;
-			}
-		}
-
-		.banner {
-			image {
-				width: 100%;
-			}
+		& > view {
+			margin: 6rpx 0;
 		}
 	}
+
+	.form {
+		padding: 20rpx;
+		background-color: #fff;
+
+		input {
+			padding: 0 20rpx;
+			height: 80rpx;
+			border: 2rpx solid #244fd6;
+			border-radius: 20rpx;
+		}
+
+		button {
+			margin-top: 30rpx;
+			height: 80rpx;
+			background: #244fd6;
+			border-radius: 40rpx;
+			color: #fff;
+		}
+	}
+
+	.banner {
+		image {
+			width: 100%;
+		}
+	}
+}
 </style>
